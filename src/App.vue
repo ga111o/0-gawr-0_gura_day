acca2305435d92c5dfc87036bc4c4114363d58f6
ga111o <ga111o@proton.me>
2025-04-01 20:53:49 +0900

    ✨ (Gawr) Gura Day!

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <a
+      href="https://namu.wiki/w/%EA%B0%80%EC%9A%B0%EB%A5%B4%20%EA%B5%AC%EB%9D%BC"
+      target="_blank"
+    >
+      <img src="/image.png" class="logo" alt="ㅋㅋ" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  width: 80%;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
